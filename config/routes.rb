e1bd7919572db3473ab9bd29de9d620fc52096e0
Chris <handleychris89@gmail.com>
2019-09-27 06:58:23 +0000

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
-    resources :lessons, only: [:show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update] 
-        resources :lessons, only: [:new, :create]
-        resources :sections, only: [] do
-      end
-      resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
+    resources :sections, only: [:update] do
+      resources :lessons, only: [:create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:create]
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
